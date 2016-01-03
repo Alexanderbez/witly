6a429a4b50cf70be883c61ff7d5ab00c3fe2db51
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-01-03 18:01:37 -0500

    Restructured config module

--- config/index.js
+++ config/index.js
@@ -2,8 +2,9 @@
 'use strict';
 
 module.exports = {
-  bootstrap: require('./bootstrap'),
-  properties: require('./properties'),
   logger: require('./logger'),
-  router: require('./router')
+  router: require('./router'),
+  properties: require('./properties'),
+  services: require('./services'),
+  bootstrap: require('./bootstrap')
 };
