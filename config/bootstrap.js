6943ed81f595ad528a5dba4622d32aae1a4106aa
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-01-03 18:02:27 -0500

    Add Express validation middleware to bootstrap call

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -6,12 +6,13 @@
  * Handles all common Express configuration and setting management.
  */
 
-const bodyParser = require('body-parser');
-const morgan     = require('morgan');
-const cors       = require('cors');
-const properties = require('./properties');
-const router     = require('./router');
-const services   = require('./services');
+const bodyParser       = require('body-parser');
+const morgan           = require('morgan');
+const cors             = require('cors');
+const properties       = require('./properties');
+const router           = require('./router');
+const services         = require('./services');
+const expressValidator = require('express-validator');
 
 module.exports = (app) => {
   /* Service and API client initialization */
@@ -27,6 +28,9 @@ module.exports = (app) => {
 
   // Parse application/json
   app.use(bodyParser.json());
+
+  // Express data validation
+  app.use(expressValidator());
 
   // HTTP logger
   app.use(morgan('common', {
