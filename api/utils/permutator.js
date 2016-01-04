d6ec7a0bd47a9ff0a1690596f38408faba7f9323
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-01-03 23:03:28 -0500

    Implement permutator class

---
+++ api/utils/permutator.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * api/utils/permutator.js
+ * 
+ * A permutator class that is responsible for picking a unique and random
+ * character sequence for a Shorty resource. This will represent a shortened
+ * URL as an UID.
+ */
+
+const _ = require('lodash');
+
+/**
+ * Permutator class constructor. A Permutator class requires a seed (n), these
+ * will represent the character set to choose from and a value k that will
+ * represent the number of characters to pick to form a UID.
+ *
+ * The are (N + k)! / (N - 1)!k!, where N is the number of possible characters
+ * to choose from, possibilites.
+ *
+ * @param {Array} n
+ * @param {Integer} k
+ * @return {Object}
+ */
+function Permutator (n, k) {
+  this.n = n || _.flatten([
+    'abcdefghijklmnopqrstuvwxyz'.split(''),
+    'abcdefghijklmnopqrstuvwxyz'.split('').map((c) => {
+      return c.toUpperCase();
+    }),
+    '0123456789'.split('')
+  ]);
+  this.k = k || 8;
+}
+
+/**
+ * Get a random permutation of length k from the sample n.
+ *
+ * @return {String}
+ */
+Permutator.prototype.next = function () {
+  return _.sample(this.n, this.k).join('');
+};
+
+module.exports = Permutator;
