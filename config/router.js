16073663fbfcd2ed0b4b70b457195f334ab8d288
Alex Bezobchuk <abezobchuk@jibe.com>
2016-01-06 15:54:58 -0500

    Add Swagger doc route to fetch Swagger docs

--- config/router.js
+++ config/router.js
@@ -7,10 +7,14 @@
  */
 
 const express = require('express');
+const apiDocs = require('./apiDocs');
 
 module.exports.bootstrap = (app) => {
   let api = require('../api');
   
+  app.use('/swagger.json', (req, res) => {
+    res.status(200).json(apiDocs);
+  });
   app.use('/api/shorties', api.controllers.shorty);
 
   return app;
