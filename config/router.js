c018665b1e6d0951b67a4ea05f4389f916d3cc2b
Alex Bezobchuk <abezobchuk@jibe.com>
2016-01-06 13:13:39 -0500

    Cleanup config/router

--- config/router.js
+++ config/router.js
@@ -7,12 +7,10 @@
  */
 
 const express = require('express');
-const appRoot = require('app-root-path');
 
 module.exports.bootstrap = (app) => {
   let api = require('../api');
   
-  // app.use('/', express.static(appRoot + '/public'));
   app.use('/api/shorties', api.controllers.shorty);
 
   return app;
