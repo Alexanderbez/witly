da9d2b66b23170c41a2a26803449a68e3b0d87a5
Alex Bezobchuk <abezobchuk@jibe.com>
2016-01-06 13:11:46 -0500

    Implement GET @ /api/shorties/search endpoint

--- api/controllers/shorty.js
+++ api/controllers/shorty.js
@@ -78,11 +78,22 @@ router.get('/find', (req, res) => {
  * @GET
  * Retrieves for all shorty resources that match a specified search criteria.
  * The search criteria should crudely be based upon existing shorty resource
- * attributes.
+ * attributes. If no resources are found, an empty collection is returned.
  */
 router.get('/search', (req, res) => {
   let query = req.query;
   log.debug(`Attempting to find shorty resources by: ${JSON.stringify(query)}`);
+
+  Shorty.find(query)
+    .then((shorties) => {
+      res.status(200).json(shorties);
+    })
+    .catch((err) => {
+      log.error(`Failed to query for shorty resources: ${err.message}`);
+      res.status(400).json({
+        error: err.message
+      });
+    });
 });
 
 /**
