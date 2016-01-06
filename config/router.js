73ea1ed2f4219af06d6a27ac872dd453f33a009f
Alex Bezobchuk <abezobchuk@jibe.com>
2016-01-06 15:56:19 -0500

    Update config/router.js doc

--- config/router.js
+++ config/router.js
@@ -12,9 +12,12 @@ const apiDocs = require('./apiDocs');
 module.exports.bootstrap = (app) => {
   let api = require('../api');
   
+  // Swagger documentation
   app.use('/swagger.json', (req, res) => {
     res.status(200).json(apiDocs);
   });
+
+  // Shorty resource API
   app.use('/api/shorties', api.controllers.shorty);
 
   return app;
